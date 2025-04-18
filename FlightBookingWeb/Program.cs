0512f5ec61c2fcb8093746db00b77f87fe43bb30
Bảo Nguyễn Phan Anh <nguyenphananhbao@gmail.com>
2025-04-18 11:34:32 +0700

    testloginandregister

--- FlightBookingWeb/Program.cs
+++ FlightBookingWeb/Program.cs
@@ -9,12 +9,13 @@ namespace FlightBookingWeb
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 
             // Configure the HTTP request pipeline.
