b24530b5b8ad71b3a465fb9310e67ff1e538b6b0
Viettrung <nguyenviettrung2005@gmail.com>
2025-05-14 13:12:12 +0700

    fix db 2nd

--- FlightBookingWeb/Models/Baggage.cs
+++ FlightBookingWeb/Models/Baggage.cs
@@ -1,32 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace FlightBookingWeb.Models;
 
-[Table("Baggage")]
 public partial class Baggage
 {
-    [Key]
-    [Column("BaggageID")]
     public int BaggageId { get; set; }
 
-    [Column("TicketID")]
     public int TicketId { get; set; }
 
-    [Column(TypeName = "decimal(5, 2)")]
     public decimal Weight { get; set; }
 
-    [Column(TypeName = "decimal(10, 2)")]
     public decimal Fee { get; set; }
 
-    [StringLength(20)]
-    [Unicode(false)]
     public string? Status { get; set; }
 
-    [ForeignKey("TicketId")]
-    [InverseProperty("Baggages")]
     public virtual Ticket Ticket { get; set; } = null!;
 }
--- FlightBookingWeb/Models/Seat.cs
+++ FlightBookingWeb/Models/Seat.cs
@@ -1,40 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace FlightBookingWeb.Models;
 
-[Table("Seat")]
-[Index("AirplaneId", "SeatNumber", Name = "UQ_Seat_Airplane", IsUnique = true)]
 public partial class Seat
 {
-    [Key]
-    [Column("SeatID")]
     public int SeatId { get; set; }
 
-    [Column("AirplaneID")]
     public int AirplaneId { get; set; }
 
-    [StringLength(10)]
-    [Unicode(false)]
     public string SeatNumber { get; set; } = null!;
 
-    [StringLength(20)]
-    [Unicode(false)]
     public string SeatClass { get; set; } = null!;
 
-    [StringLength(20)]
-    [Unicode(false)]
     public string SeatType { get; set; } = null!;
 
     public bool? IsEmergencyExit { get; set; }
 
-    [ForeignKey("AirplaneId")]
-    [InverseProperty("Seats")]
     public virtual Airplane Airplane { get; set; } = null!;
 
-    [InverseProperty("Seat")]
     public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
 }
